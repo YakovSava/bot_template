ebd87324a695ea2b582c9861cff0cd6615a80c9f
Yakov Savelev (Laptop) <g6h6m238929@gmail.com>
2023-06-25 14:01:54 +0500

    Version 0.8
    Beta version

---
+++ Crandom.py
@@ -0,0 +1,58 @@
+from rand import randint, choice
+
+def shuffle(lst:list) -> list:
+	'''
+	Shuffle the list!
+	'''
+	new_list = []
+	for item in lst:
+		rindex = randint(0, len(lst))
+		new_list.append(lst.pop(rindex))
+	return new_list
+
+def randrange(range_min:int, range_max:int) -> range:
+	"""
+	Get random range
+	"""
+	while True:
+		try: return range(
+				randint(range_min, range_max // 2),
+				randint(range_min, range_max)
+			)
+		except: continue
+
+def nrandint(range_min:int, range_max:int, n:int) -> list[int]:
+	"""
+	Return random list with "n"
+	"""
+	return [randint(range_min, range_max) for _ in range(n)]
+
+def nchoice(lst:list, n:int) -> list:
+	"""
+	Return random list with "n" with items from lst
+	"""
+	return [choice(lst) for _ in range(n)]
+
+if __name__ == '__main__':
+	for _ in range(5):
+		print(randint(0, 100))
+	print("-"*30)
+
+	for _ in range(5):
+		print(choice(["apple", "banana", "pineapple"]))
+	print("-"*30)
+
+	print(shuffle(["apple", "banana", "pineapple"]))
+	print("-"*30)
+
+	for _ in range(5):
+		print(randrange(0, 100))
+	print("-"*30)
+
+	for _ in range(5):
+		print(nrandint(0, 100, 5))
+	print("-"*30)
+
+	for _ in range(5):
+		print(nchoice(["apple", "banana", "pineapple"], 5))
+	print("-"*30)
--- __init__.py
+++ __init__.py
@@ -8,11 +8,9 @@ else:
 	system('python3 compiler.py build_ext --inplace')
 
 for file in listdir():
-	if file.endswith((
-		'.c', '.h', '.cc',
-		'.hpp', '.cpp', '.cxx',
-		'.h++', '.hh', '.hxx'
-	)):
+	if file.endswith('.pyd'):
 		exec(f"import {file.split('.')[0]}")
 
 from recomendate import recomendate
+from Crandom import choice, randint, nrandint,\
+	randrange, shuffle, nchoice
