860980c0287e085ad59b56b259bd7dd8b6ce04e0
Jacob Savel'ev <g6h6m238929@gmail.com>
2023-06-27 17:11:27 +0500

    Version 0.10

--- __init__.py
+++ __init__.py
@@ -1,11 +1,14 @@
 from sys import platform
 from os import listdir, system
 from jsexec import JSeval, JSexec
+from gcc_clang_compiler import compile
 
 if platform == 'win32':
 	system('python compiler.py build_ext --inplace')
 else:
 	system('python3 compiler.py build_ext --inplace')
+
+compile()
 
 for file in listdir():
 	if file.endswith('.pyd'):
